062d288b75196b2d109d14de4916988677777b54
si-manann-sseth <manan.sheth@sportzinteractive.net>
2024-09-12 17:19:38 +0530

    Init for Host App

---
+++ ios-host/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-host
+//
+//  Created by Manann Sheth on 12/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-host/ios_hostApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_hostApp.swift
+//  ios-host
+//
+//  Created by Manann Sheth on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_hostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
